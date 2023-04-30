6297deee71108d00add26142c2ee1a12f891bd7a
Alvaro <alvaromosquerarial@hotmail.com>
2023-04-30 15:54:12 +0200

    con comentarios y pajina de explicaión

--- www/ud07/symfony_projects/src/Controller/TiendaController.php
+++ www/ud07/symfony_projects/src/Controller/TiendaController.php
@@ -7,19 +7,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TiendaController extends AbstractController {
 
+    //controller de la ruta principal que dirije al home de la pajina
     #[Route('/')]
     public function homepage() {
-        return $this->render('tienda/homepage.html.twig', [
-            'imagen' => 'symfony_white_03.svg',
-        ]);    
+        return $this->render('tienda/homepage.html.twig', []);    
     }
-    
+    //controller quw dirige a la parte de la tienda de la pajina 
+   
     #[Route('/tienda')]
     public function tiendaPage() {
-        return $this->render('tienda/tiendapage.html.twig', [
-            'imagen' => '/symfony_white_03.svg',
-        ]);    
+        return $this->render('tienda/tiendapage.html.twig', []);    
     }
+
+//controller que dirige a la parte de los precios
+//si se redirige desde los cursos de programación se aplica un 50% de descuento y se cambian los precios
+//si es de otros cursos no hace nada, imprime los precios normales.
+
     #[Route('/precio/{slug}')]
     public function Preciopage(string $slug=null) {
         if($slug=="programacion"){
@@ -41,6 +44,10 @@ class TiendaController extends AbstractController {
     }  
    
 
+    }
+    #[Route('/guia')]
+    public function giapage() {
+        return $this->render('tienda/guiapage.html.twig', []);    
     }
     
 }
